3073d3c07061cb44684f1cb497d4ecce418ae67f
ankhenhhh <akrobinsonvip3084@gmail.com>
2024-10-13 23:27:01 +0700

    add view and search train, fix crud train

--- SWPProject/src/java/dal/DBContext.java
+++ SWPProject/src/java/dal/DBContext.java
@@ -19,7 +19,7 @@ public class DBContext {
     public  DBContext(){
         String url="jdbc:mysql://localhost:3306/trainproject";
         String user = "root";
-        String pass = "Kimanh3082@";
+        String pass = "123";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, pass);
